d5f49790bc6f1ad41dd0e4e60e20a0eb698c291d
YazAtth <yasith.athan@gmail.com>
2022-06-06 23:12:04 +0100

    setup

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("hello");
+// Express modules
+const express = require("express");
+const app = express();
+// Express ejs layouts modules
+const expressLayouts = require("express-ejs-layouts");
+
+// CONFIG
+app.set("view engine", "ejs"); // Allows us to use ejs instead of html files in the views folder
+app.use(express.static(__dirname + "/public")); // Makes the "public" folder which contains the css available to the browser.
+app.use(expressLayouts);
+
+
+
+
+
+// ROUTES
+const articlesRoute = require("./routes/articles");
+app.use("/articles", articlesRoute);
+
+
+app.get("/", (req, res) => {
+    res.render("home", {title: "Home"});
+})
+
+
+app.listen(3001);
+
+
