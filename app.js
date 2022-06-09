b3f555b5846486dca498404085f000dbb32e40ff
YazAtth <yasith.athan@gmail.com>
2022-06-09 10:39:02 +0100

    displays articles from the database, doesn't update however

--- app.js
+++ app.js
@@ -3,26 +3,47 @@ const express = require("express");
 const app = express();
 // Express ejs layouts modules
 const expressLayouts = require("express-ejs-layouts");
+// Mongoose
+const mongoose = require("mongoose");
+
+const PORT = 3001;
 
 // CONFIG
 app.set("view engine", "ejs"); // Allows us to use ejs instead of html files in the views folder
 app.use(express.static(__dirname + "/public")); // Makes the "public" folder which contains the css available to the browser.
 app.use(expressLayouts);
+require("dotenv/config")
+
+
 
 
 
 
 
 // ROUTES
-const articlesRoute = require("./routes/articles");
+const articlesRoute = require("./routes/articlesRoutes");
 app.use("/articles", articlesRoute);
 
 
 app.get("/", (req, res) => {
-    res.render("home", {title: "Home"});
+    res.render("home", {title: "Home", pageTitle: "Home"});
 })
 
 
-app.listen(3001);
+// Connect to DB
+// First argument is the Mongoose URI which is held in the .env file
+// Ensure you put the Collection Name before the ? in the URI 
+mongoose.connect(process.env.DB_CONNECTION, () => {
+    console.log("Connected to DB!");
+})
+
+app.listen(PORT, "localhost", () => {
+    console.log(`Server hosted at --> http://localhost:${PORT}`);
+});
+
+
+
+
+
 
 
