1f6771463819b3526d1878e306f99f7cf15ab8ed
YazAtth <yasith.athan@gmail.com>
2022-06-09 18:31:57 +0100

    utilised the mongoose 'exists()' function to identify duplicates faster and more concisely

--- routes/articlesRoutes.js
+++ routes/articlesRoutes.js
@@ -10,10 +10,9 @@ const apiURL = 'https://www.theguardian.com/international/rss';
 
 
 // Makes requests and updates database every 15 minutes
-schedule.scheduleJob("*/15 * * * *", async () => {
-    console.log("ran");
+schedule.scheduleJob("*/60 * * * * *", async () => {
 
-    dbData = await requestsController.requestFromUrl(apiURL)
+    dbData = await requestsController.requestFromUrl(apiURL) // Returns data from the News Sources formatted as a JSON object
 
     for (let i=0; i<dbData.length; i++) {
         const post = new Article({
@@ -21,80 +20,31 @@ schedule.scheduleJob("*/15 * * * *", async () => {
             description: dbData[i].description[0],
             link: dbData[i].link[0],
             pubDate: dbData[i].pubDate[0],
-            "dc:creator": dbData[i]["dc:creator"][0],
-            "itemID": "[" + dbData[i].pubDate + "][" + dbData[i].title + "]"
         });
     
-
-        // Below, stops entries from being entered more than once into the db
-
-        // Post.find runs asynchrounously so the variable "i" will be evaluated at the end when it is 2 instead of iterating.
-        // Making the "newPost" variable ensures this doesn't happen.
         newPostTitle = dbData[i].title[0];
         newPostDate = dbData[i].pubDate[0];
 
-    
+        const sameArticleTitleExists = await Article.exists({title: newPostTitle});
+        const sameArticleDateExists = await Article.exists({pubDate: newPostDate});
 
-        let titleArr = [];
-        let dateArr = [];
-        let updatedArticlePtr;
-
-
-        const dbMongo = await Article.find({}, (err, articlesInCollection) => {
-            // console.log(`Title: ${newPostTitle}`);
-            // console.log("start two");
-            if(err) console.log(err);
-        }).clone()
-            .then(result => {
-            
-                // Iterates through every item in the db collection and adds the itemID of every item into an array
-                result.map(singleArticle => {
-                    // itemIdArr.push(singlePost.itemID); 
-                    titleArr.push(singleArticle.title);  
-                    dateArr.push(singleArticle.pubDate);                
-                })
-
-                let isDuplicates = false;
-                let hasUpdate = false;
-
-                // Compares the itemID of the new item with the itemID of all the existing items in the db
-                // If a duplicate is found, "isDuplicates" is set to "true" 
-
-                for (j=0; j<titleArr.length; j++) {  
-                    // console.log(`looking at ${titleArr[j]} AND ${newPostTitle}`);
-                    
-                    if (titleArr[j] == newPostTitle && dateArr[j] == newPostDate) {
-                        isDuplicates = true;
-                        // console.log(`${titleArr[j]} == ${newPostTitle}`);
-                        console.log("found duplicate");
-                        break;
-                    } else if (titleArr[j] == newPostTitle) {
-                        hasUpdate = true;
-                        console.log(`post has an update: ${newPostTitle}`);
-                        updatedArticlePtr = titleArr[j];
-                        break;
-                    }
-                }
-
-
-
-                // Ensures a post will be saved only if it is not a duplicate
-                if (isDuplicates === false && hasUpdate === false) {
-                    post.save(); // uploads the post to the database
-                    // console.log("POST WAS SAVED");
-                }
-                else if (hasUpdate === true ){
-                    Article.deleteMany({title: updatedArticlePtr}) // Deletes old versions of article
+        if (sameArticleTitleExists && sameArticleDateExists) { // If an article already exists in the db with the same title and date: marked as duplicated
+            // console.log("Duplicate Found");
+        }
+        else if (sameArticleTitleExists && !sameArticleDateExists) { // An an article exists in the db with the same title but different date: marked as an update for the article
+            console.log("Article with Update Found");
+            Article.deleteMany({title: newPostTitle}) // Deletes old versions of article
                         .then(result => {
                             post.save(); // Saves new versions of article
-                            console.log("Updated Post");
                         });
-                }           
-            });
+        }
+        else { // Otherwise marked as a new article
+            console.log("New Article Found");
+            post.save();
+        }
     }
     console.log("Finished schedule");
 });
-
 
 
 
