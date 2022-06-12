70b73e21763acb461e79595a9a4f0977dbcde0f6
YazAtth <yasith.athan@gmail.com>
2022-06-12 23:02:06 +0100

    api urls are now read in from JSON file

--- routes/articlesRoutes.js
+++ routes/articlesRoutes.js
@@ -2,19 +2,27 @@ const { clearCache } = require("ejs");
 const express = require("express");
 const router = express.Router();
 const schedule = require("node-schedule");
+const fs = require("fs");
 const { exit } = require("process");  // Temporary for debugging
 
+
 const requestsController = require("../controllers/requestsController");
-const Article = require("../models/Article");
+const Article = require("../models/Article"); // Import schema for each Article for the db
 
-let apiUrlList = ['https://www.theguardian.com/international/rss', 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml', 'http://feeds.bbci.co.uk/news/england/london/rss.xml'];
+// Takes the URLs from the JSON file and turns them into an array
+newsFeedSources = JSON.parse(fs.readFileSync("newsSourceList.json"));
+jsonUrlList = newsFeedSources.urls;
+apiUrlList = [];
+for (let i=0; i<jsonUrlList.length; i++) {
+    apiUrlList.push(jsonUrlList[i]);
+}
 
-lastUpdated = undefined;
+lastUpdated = undefined;  // Displayed to the user to show when the news feed was last updated (eg. 14 minutes ago)
 
 
 // Makes requests and updates database every 15 minutes
-schedule.scheduleJob("*/15 * * * *", async () => {
-// schedule.scheduleJob("*/30 * * * * *", async () => {
+// schedule.scheduleJob("*/15 * * * *", async () => {
+schedule.scheduleJob("*/30 * * * * *", async () => {
     console.log("Started schedule");
 
     promiseList = [];
@@ -24,14 +32,13 @@ schedule.scheduleJob("*/15 * * * *", async () => {
     }
 
     lastUpdated = Date.now();
-    Promise.allSettled(promiseList);
+    Promise.allSettled(promiseList); // Takes all of the promises and creates one big promise
 
 });
 
 
 
 router.get("/", async (req, res) => {
-    console.log("ran");
 
     //TODO Sort/filter has to be done around here
     Article.find().sort({pubDate: -1})
@@ -51,7 +58,16 @@ router.get("/json", async (req, res) => {
 });
 
 router.get("/runtest", async (req, res) => {
-    console.log(Date.now());
+    newsFeedSources = JSON.parse(fs.readFileSync("newsSourceList.json"));
+    urlList = newsFeedSources.urls;
+
+    urlArray = [];
+
+    for (let url in urlList) {
+        urlArray.push(urlList[url]);
+    }
+    
+    console.log(urlArray[0]);
 
     res.send("lol");
 })
